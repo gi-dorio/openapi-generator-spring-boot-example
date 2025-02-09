6cee9abb5839b7e15f55690a5b76affbbaade8b5
GiuseppeCSI <giuseppe.dorio@csi.it>
2025-02-09 21:50:37 +0100

    better management of spring security

--- src/main/java/com/example/openapiexample/config/SpringSecurityConfig.java
+++ src/main/java/com/example/openapiexample/config/SpringSecurityConfig.java
@@ -1,15 +1,25 @@
 package com.example.openapiexample.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Component
+
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-            .requestMatchers(new AntPathRequestMatcher("/**"));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests((authorize)->{
+                    authorize.requestMatchers("/**").permitAll();
+
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
